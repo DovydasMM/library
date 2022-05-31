c7a28f73be77c8e6adce66604a5adda853cd7512
DovydasMM <dovistk@gmail.com>
2022-05-31 11:59:58 +0300

    Added sidebar with book addition

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector(".container");
+const form = document.getElementById('addBook');
 let myLibrary = [];
 
 function Book(title,author, numberOfPages, status) {
@@ -14,47 +16,74 @@ function addBookToLibrary() {
 }
 
 
-let book1 = new Book("Tarp pilku debesu","Ruta Sepetys", 367, "fully read");
-const container = document.querySelector(".container");
+//Function for DOM manipulation. It takes info from 
+function cardCreation(title, author, numberOfPages){
+    let card=document.createElement('div');
+    card.classList.add('card')
 
+    //title
+    titleOfBook=document.createElement('div');
+    titleOfBook.classList.add('title')
+    titleOfBook.textContent="The title is: "+ title
 
-
-
-
-function cardCreation(pavad, autorius, psl){
-    let knyga1=document.createElement('div');
-    knyga1.classList.add('card')
+    //author
+    authorOfBook=document.createElement('div');
+    authorOfBook.classList.add('author')
+    authorOfBook.textContent="Author: " +author;
     
-    pavadinimas=document.createElement('div');
-    pavadinimas.classList.add('title')
-    pavadinimas.textContent="The title is: "+ pavad
-    knyga1.appendChild(pavadinimas)
+    //pages
+    pageCount=document.createElement('div');
+    pageCount.classList.add('pages')
+    pageCount.textContent=numberOfPages + " pages";
     
-    authoriuss=document.createElement('div');
-    authoriuss.classList.add('author')
-    authoriuss.textContent="Author " +autorius;
-    knyga1.appendChild(authoriuss)
-    
-    puslapis=document.createElement('div');
-    puslapis.classList.add('puslapiai')
-    puslapis.textContent=psl + " pages";
-    knyga1.appendChild(puslapis)
-    container.appendChild(knyga1);
+    //appending info to cards
+    card.appendChild(titleOfBook)
+    card.appendChild(authorOfBook)
+    card.appendChild(pageCount)
+
+    //appends the created card to the container
+    container.appendChild(card);
+}
+
+const book2 = new Book(" Don Quixote","Miguel de Cervantes", 1077, "not read");
+const book3 = new Book("1984","George Orwell", 328, "fully read");
+const book4 = new Book("The Count of Monte Cristo","Alexandre Dumas", 884, "fully read");
+
+
+myLibrary.push(book2)
+myLibrary.push(book3)
+myLibrary.push(book4)
+
+
+function fillContainer(){
+    for (book in myLibrary){
+        cardCreation(myLibrary[book].title, myLibrary[book].author,myLibrary[book].numberOfPages)
+    }
+}
+fillContainer();
+
+const bookTitle = form.elements['bookTitle']
+const bookAuthor = form.elements['bookAuthor']
+const bookPageCount = form.elements['pageCount']
+
+function addNewBook(){
+    let newBookIndex=myLibrary.length-1;
+    cardCreation(myLibrary[newBookIndex].title, myLibrary[newBookIndex].author,myLibrary[newBookIndex].numberOfPages)
 }
 
 
 
-const book2 = new Book("Tarp pilku debesu","Ruta Sepetys", 367, "fully read");
-const book3 = new Book("Tbaba","xxxtys", 4444, "fully read");
-const book4 = new Book("Tadssdu debesu","RdsadasdSepetys", 711, "fully read");
 
 
+const buttonSubmit = document.querySelector('.buttonSubmit')
+buttonSubmit.addEventListener('click', () => {
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPageCount.value)
+    myLibrary.push(newBook);
+    addNewBook();
+    bookTitle.value ='';
+    bookAuthor.value='';
+    bookPageCount.value ='';
+    
+});
 
 
-
-cardCreation(book2.title,book2.author,book2.numberOfPages)
-cardCreation(book3.title,book3.author,book3.numberOfPages)
-cardCreation(book4.title,book4.author,book4.numberOfPages)
-
-
-console.log(book4.title)
